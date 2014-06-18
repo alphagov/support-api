ef4495369f42be4a43f2c5218fc8310e17814d7e
Jake Benilov <benilov@gmail.com>
2014-06-18 18:08:05 +0100

    Add airbrake gem for errbit error reporting

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '4.1.1'
 gem 'rails-api', '0.2.1'
 gem 'sqlite3'
 gem 'logstasher', '0.5.3'
+gem 'airbrake', '3.1.15'
 
 group :development do
   gem 'spring', '1.1.3'
---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,5 @@
+Airbrake.configure do |config|
+  # Adding "production" to the development environments causes Airbrake not
+  # to attempt to send notifications.
+  config.development_environments << "production"
+end
