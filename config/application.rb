7cf7c6c02df9ed84535f86fbbdf5be931f3b685c
Jake Benilov <benilov@gmail.com>
2014-06-19 11:47:43 +0000

    Tell rails-api to use standard rails middleware
    Otherwise the app doesn't boot up.

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module SupportApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.api_only = false
   end
 end
