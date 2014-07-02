3e60a5c38f1a7397f87e4f0e0e58ef1d5f96104a
Jake Benilov <benilov@gmail.com>
2014-07-02 09:51:29 +0100

    Move simplecov requires above `config/environment`
    See https://github.com/colszowka/simplecov#getting-started for details

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,11 +4,6 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
-require 'simplecov'
-require 'simplecov-rcov'
-SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
-SimpleCov.start 'rails'
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
