c80112e0c5e63986c3e6bd54bfe3e633a7eaa50c
Jake Benilov <benilov@gmail.com>
2014-07-08 16:37:01 +0100

    Add DB schema so that test DBs can be created

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,30 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control system.
+# It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(:version => 20140630065039) do
+
+  create_table "anonymous_contacts", :force => true do |t|
+    t.string   "type"
+    t.text     "what_doing"
+    t.text     "what_wrong"
+    t.text     "details"
+    t.string   "source"
+    t.string   "page_owner"
+    t.text     "url"
+    t.string   "user_agent"
+    t.string   "referrer"
+    t.boolean  "javascript_enabled"
+    t.datetime "created_at",                                    :null => false
+    t.datetime "updated_at",                                    :null => false
+    t.string   "personal_information_status"
+    t.string   "slug"
+    t.integer  "service_satisfaction_rating"
+    t.text     "user_specified_url"
+    t.boolean  "is_actionable",               :default => true, :null => false
+    t.string   "reason_why_not_actionable"
+    t.text     "path"
+  end
 
 end
