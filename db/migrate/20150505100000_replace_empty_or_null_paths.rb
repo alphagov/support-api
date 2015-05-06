c785c1fb6c7ee160ab8ce3bcd59f69a735040887
Jake Benilov <benilov@gmail.com>
2015-05-06 11:02:15 +0100

    Migration to update empty paths to "/"
    This is necessary prior to adding a not-null constraint on the path
    column.
    
    Migration `20150430133750_replace_empty_paths.rb` didn't go quite far
    enough to catch all of these records.
    
    The timestamp on the new migration is before `20150505162618_path_not_null.rb`
    so it runs before that one.

---
+++ db/migrate/20150505100000_replace_empty_or_null_paths.rb
@@ -0,0 +1,11 @@
+class ReplaceEmptyOrNullPaths < ActiveRecord::Migration
+  def up
+    Support::Requests::Anonymous::AnonymousContact.
+      unscoped.
+      where("path = '' or path is null").
+      update_all(path: "/")
+  end
+
+  def down
+  end
+end
