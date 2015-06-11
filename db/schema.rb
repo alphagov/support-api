4628a9226643927c65da4d500e3c3c104672c002
Paul Bowsher <paul.bowsher@gmail.com>
2015-06-11 14:43:21 +0100

    Remove feedback records with invalid dates
    There was a short scan of GOV.UK in November 2012 using the Vega tool.
    This populated some feedback forms various maliciously crafted strings
    and submitted them. There doesn’t seem to be any side effects from
    this, except that somehow created_at and updated_at dates were set to
    blank for just 3 of the records.
    
    The information provided is meaningless, so this migration simply
    deletes the records.
    
    It will find other records with blank (not NULL) dates and delete them,
    but as a safeguard it’ll just refuse to do anything if it finds more
    than 10 records.
    
    This work is needed because attempting to import the data into Postgres
    in this state will make it fail the NOT NULL constraints on these
    columns.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150604140707) do
+ActiveRecord::Schema.define(version: 20150611133227) do
 
   create_table "anonymous_contacts", force: :cascade do |t|
     t.string   "type",                        limit: 255
