f89d37f5ec2a3f736f736479312f77b2f96e71c4
Paul Bowsher <paul.bowsher@gmail.com>
2015-06-12 17:17:34 +0100

    Use db:reset which uses the correct schema format

--- jenkins.sh
+++ jenkins.sh
@@ -7,6 +7,6 @@ git clean -fdx
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 
 for db_type in mysql postgresql; do
-  SUPPORT_API_DB_TYPE=$db_type bundle exec rake db:drop db:create db:schema:load
+  SUPPORT_API_DB_TYPE=$db_type bundle exec rake db:reset
   SUPPORT_API_DB_TYPE=$db_type bundle exec rake ci:setup:rspec default --trace
 done
