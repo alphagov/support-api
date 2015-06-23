1955fb0d5ab9ce57ea7028ed5a95e83dd820be4d
Paul Bowsher <paul.bowsher@gmail.com>
2015-06-23 16:22:48 +0100

    Add index on path using PostgreSQL's varchar_pattern operator class

---
+++ db/migrate/20150623151655_add_path_index_with_varchar_pattern_ops.rb
@@ -0,0 +1,19 @@
+class AddPathIndexWithVarcharPatternOps < ActiveRecord::Migration
+  def up
+    remove_index :anonymous_contacts, :path
+
+    if SupportApi.postgresql?
+      execute <<-SQL
+        CREATE INDEX index_anonymous_contacts_on_path
+        ON anonymous_contacts USING btree (path varchar_pattern_ops);
+      SQL
+    else
+      add_index :anonymous_contacts, [:path], length: {path: 128}
+    end
+  end
+
+  def down
+    remove_index :anonymous_contacts, [:path]
+    add_index :anonymous_contacts, [:path], length: {path: 128}
+  end
+end
