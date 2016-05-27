ee10cf91963667a5a1349b6756fde07dfacf3f48
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2016-05-27 15:37:51 +0000

    Share aggregated service feedback type through API

--- app/models/aggregated_service_feedback.rb
+++ app/models/aggregated_service_feedback.rb
@@ -4,4 +4,15 @@ class AggregatedServiceFeedback < AnonymousContact
     "aggregated-service-feedback"
   end
 
+  def as_json(options = {})
+    attributes_to_serialise = [
+      :type, :path, :id, :created_at, :slug,
+      :service_satisfaction_rating, :details,
+    ]
+    super({
+      only: attributes_to_serialise,
+      methods: :url,
+    }.merge(options))
+  end
+
 end
