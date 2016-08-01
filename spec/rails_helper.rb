fd0a9b4b4c46b90b4e8b8b2b95ba74406043a1a9
Tom Russell <tom.russell@digital.cabinet-office.gov.uk>
2016-08-01 15:40:04 +0000

    Bring dependencies up to date
    Let's be good citizens and as we touch the application bring its dependencies
    up to date.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,4 +40,9 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  RSpec.configure do |config|
+    config.include(Shoulda::Matchers::ActiveModel, type: :model)
+    config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  end
 end
