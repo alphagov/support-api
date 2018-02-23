858cfe4af7d0cf6d26c37d6eec1e219ab267f5d1
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2018-02-23 14:45:12 +0000

    Added Pagination to the /feedback-by-day Endpoint
    Added pagination and summary information about the pagination to the response.

--- app/controllers/feedback_by_day_controller.rb
+++ app/controllers/feedback_by_day_controller.rb
@@ -1,15 +1,24 @@
 require 'date_parser'
 
 class FeedbackByDayController < ApplicationController
+  before_action :validate_params!, only: :index
+
   def index
-    unless date
-      head :bad_request
-      return
-    end
-    render json: {results: FeedbackByDay.retrieve(date)}
+    render json: FeedbackByDay.retrieve(date, params[:page], params[:per_page])
   end
 
 private
+
+  def validate_params!
+    head :bad_request unless valid_params?
+  end
+
+  def valid_params?
+    return false if params[:page] && !params[:page].match(/^\d+$/)
+    return false if params[:per_page] && !params[:per_page].match(/^\d+$/)
+    date
+  end
+
   def date
     DateParser.parse(params[:date])
   end
--- spec/requests/feedback_by_day_spec.rb
+++ spec/requests/feedback_by_day_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe "/feedback-by-day endpoint" do
   before :each do
-    create_list(:problem_report, 5, path: "/browse/abroad", created_at: Time.utc(2018, 02, 21))
-    create_list(:problem_report, 4, path: "/browse/abroad", created_at: Time.utc(2018, 02, 22))
-    create_list(:problem_report, 6, path: "/browse/benefits", created_at: Time.utc(2018, 02, 21))
-    create_list(:problem_report, 2, path: "/browse/benefits", created_at: Time.utc(2018, 02, 22))
-    create_list(:problem_report, 8, path: "/browse/tax", created_at: Time.utc(2018, 02, 21))
-    create_list(:problem_report, 3, path: "/browse/tax", created_at: Time.utc(2018, 02, 22))
+    create_list(:problem_report, 5, path: "/browse/abroad", created_at: Time.utc(2018, 0o2, 21))
+    create_list(:problem_report, 4, path: "/browse/abroad", created_at: Time.utc(2018, 0o2, 22))
+    create_list(:problem_report, 6, path: "/browse/benefits", created_at: Time.utc(2018, 0o2, 21))
+    create_list(:problem_report, 2, path: "/browse/benefits", created_at: Time.utc(2018, 0o2, 22))
+    create_list(:problem_report, 8, path: "/browse/tax", created_at: Time.utc(2018, 0o2, 21))
+    create_list(:problem_report, 3, path: "/browse/tax", created_at: Time.utc(2018, 0o2, 22))
   end
 
   context 'with invalid requests' do
@@ -18,6 +18,16 @@ describe "/feedback-by-day endpoint" do
 
     it 'returns bad request for invalid date' do
       get_json "/feedback-by-day/2018-02-31"
+      expect(response.status).to eq(400)
+    end
+
+    it 'returns bad request for invalid page' do
+      get_json "/feedback-by-day/2018-02-02?page=blah"
+      expect(response.status).to eq(400)
+    end
+
+    it 'returns bad request for invalid per_page' do
+      get_json "/feedback-by-day/2018-02-02?per_page=blah"
       expect(response.status).to eq(400)
     end
   end
@@ -40,6 +50,12 @@ describe "/feedback-by-day endpoint" do
           'count' => 8
         }
       ])
+      expect(json_response).to include(
+        'total_count' => 3,
+        'current_page' => 1,
+        'pages' => 1,
+        'page_size' => 100
+      )
     end
 
     it 'returns the correct figures for 2018-02-22' do
@@ -59,6 +75,52 @@ describe "/feedback-by-day endpoint" do
           'count' => 3
         }
       ])
+      expect(json_response).to include(
+        'total_count' => 3,
+        'current_page' => 1,
+        'pages' => 1,
+        'page_size' => 100
+      )
+    end
+  end
+
+  context 'with pagination' do
+    it 'returns the correct figures for 2018-02-21 1st page' do
+      get_json "/feedback-by-day/2018-02-21?page=1&per_page=2"
+      expect(response.status).to eq(200)
+      expect(json_response["results"]).to eq([
+        {
+          'path' => '/browse/abroad',
+          'count' => 5,
+        },
+        {
+          'path' => '/browse/benefits',
+          'count' => 6,
+        }
+      ])
+      expect(json_response).to include(
+        'total_count' => 3,
+        'current_page' => 1,
+        'pages' => 2,
+        'page_size' => 2
+      )
+    end
+
+    it 'returns the correct figures for 2018-02-21 2nd page' do
+      get_json "/feedback-by-day/2018-02-21?page=2&per_page=2"
+      expect(response.status).to eq(200)
+      expect(json_response["results"]).to eq([
+        {
+          'path' => '/browse/tax',
+          'count' => 8
+        }
+      ])
+      expect(json_response).to include(
+        'total_count' => 3,
+        'current_page' => 2,
+        'pages' => 2,
+        'page_size' => 2
+      )
     end
   end
 end
