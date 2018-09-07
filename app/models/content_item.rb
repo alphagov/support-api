02d18d2bb406c8e895eef5287dd4ba737e3eb00a
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-09-07 10:18:33 +0100

    Reformat Ruby code for some queries

--- app/models/content_item.rb
+++ app/models/content_item.rb
@@ -13,18 +13,18 @@ class ContentItem < ApplicationRecord
     where(document_type: document_type)
   }
 
-  def self.summary(ordering="last_7_days")
+  def self.summary(ordering = "last_7_days")
     ordering_mode = ordering == "path" ? "ASC" : "DESC"
 
-    query = joins(:anonymous_contacts).
-      select("content_items.path as path").
-      select("#{last_7_days} AS last_7_days").
-      select("#{last_30_days} AS last_30_days").
-      select("#{last_90_days} AS last_90_days").
-      where("anonymous_contacts.created_at > ?", midnight_last_night - 90.days).
-      group("content_items.path").
-      having("#{last_7_days} + #{last_30_days} + #{last_90_days} > 0").
-      order("#{ordering} #{ordering_mode}")
+    query = joins(:anonymous_contacts)
+      .select("content_items.path AS path")
+      .select("#{last_7_days} AS last_7_days")
+      .select("#{last_30_days} AS last_30_days")
+      .select("#{last_90_days} AS last_90_days")
+      .where("anonymous_contacts.created_at > ?", midnight_last_night - 90.days)
+      .group("content_items.path")
+      .having("#{last_7_days} + #{last_30_days} + #{last_90_days} > 0")
+      .order("#{ordering} #{ordering_mode}")
 
     connection.
       select_all(query).
@@ -35,16 +35,16 @@ class ContentItem < ApplicationRecord
   def self.doctype_summary(ordering: "last_7_days", document_type: nil)
     ordering_mode = ordering == "document_type" ? "ASC" : "DESC"
 
-    query = joins(:anonymous_contacts).
-        select("content_items.document_type as document_type").
-        select("#{last_7_days} AS last_7_days").
-        select("#{last_30_days} AS last_30_days").
-        select("#{last_90_days} AS last_90_days").
-        where(document_type: document_type).
-        where("anonymous_contacts.created_at > ?", midnight_last_night - 90.days).
-        group("content_items.document_type").
-        having("#{last_7_days} + #{last_30_days} + #{last_90_days} > 0").
-        order("#{ordering} #{ordering_mode}")
+    query = joins(:anonymous_contacts)
+        .select("content_items.document_type as document_type")
+        .select("#{last_7_days} AS last_7_days")
+        .select("#{last_30_days} AS last_30_days")
+        .select("#{last_90_days} AS last_90_days")
+        .where(document_type: document_type)
+        .where("anonymous_contacts.created_at > ?", midnight_last_night - 90.days)
+        .group("content_items.document_type")
+        .having("#{last_7_days} + #{last_30_days} + #{last_90_days} > 0")
+        .order("#{ordering} #{ordering_mode}")
 
     connection.
         select_all(query).
