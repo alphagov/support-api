09ce5006ac5e233dbda3623a7b354d0a5bc7de1f
Isabell (Issy) Long <me@issyl0.co.uk>
2019-04-17 14:58:28 +0100

    Add a Rake task to delete old Zendesk tickets and users
    - We have a need to delete old Zendesk tickets and users, for members of
      the public who haven't contacted us in a year. This is to comply with
      privacy policies.
    - Users are created in Zendesk for every contact form submission that
      people send us. That means there are a lot (~2 million users), and
      700k tickets.
    - It's extremely slow, but that's due to the Zendesk API not having
      much usable filtering, and us iterating through _everything_, and
      also the speed of Zendesk returning results. A full run takes 24h to
      dry-run delete, but it shouldn't be this bad when we run it in
      future once we've done this bulk deletion of everything old and
      public from the last five years.
    - This uses the zendesk_api gem, the officially supported Zendesk gem.
      This app also uses the gds_zendesk gem, our own wrapper around _some of
      the functionality_, but it doesn't support deletion or more recent
      Zendesk API features, hence using the official one for this process.
    
    https://trello.com/c/MaMoUari/350-spike-script-to-delete-users-from-zendesk
    
    Co-authored-by: Aditya Pahuja <aditya.pahuja@digital.cabinet-office.gov.uk>
    Co-authored-by: Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
    Co-authored-by: Andy Paine <andy.paine@digital.cabinet-office.gov.uk>

---
+++ lib/tasks/delete_old_zendesk_users_and_tickets.rake
@@ -0,0 +1,26 @@
+require 'zendesk_api'
+require 'zendesk_cleaner'
+
+namespace :zendesk do
+  desc "Delete info in Zendesk about people who haven't contacted us in a year."
+  task :delete_old_users_and_tickets, [] do |_, args|
+    # Authenticate with Zendesk.
+    client = ZendeskAPI::Client.new do |config|
+      config.url = ENV['ZENDESK_URL']
+      config.username = ENV['ZENDESK_USER_EMAIL']
+      config.password = ENV['ZENDESK_USER_PASSWORD']
+
+      config.retry = true
+    end
+
+    cleaner = ZendeskCleaner.new(client, ENV.fetch('DRY_RUN', true), args.extras.map(&:to_i))
+
+    cleaner.display_current_num_of_tickets_and_users
+
+    cleaner.delete_old_tickets
+
+    cleaner.delete_old_users
+
+    cleaner.how_many_things_have_we_deleted
+  end
+end
---
+++ lib/zendesk_cleaner.rb
@@ -0,0 +1,83 @@
+require 'set'
+require 'time'
+
+class ZendeskCleaner
+  attr_reader :client, :users_with_existing_tickets, :tickets_to_delete, :users_to_delete
+
+  def initialize(client, dry_run = true, groups = [])
+    @client = client
+    @dry_run = dry_run
+    @groups = groups
+    @users_with_existing_tickets = Set.new
+    @tickets_to_delete = Set.new
+    @users_to_delete = Set.new
+  end
+
+  def display_current_num_of_tickets_and_users
+    puts "This Zendesk instance has #{@client.users.count} users and #{@client.tickets.count} tickets."
+  end
+
+  def how_many_things_have_we_deleted
+    puts "We have deleted #{@users_to_delete.size} users and #{@tickets_to_delete.size} tickets."
+  end
+
+  def delete_old_tickets
+    @client.tickets.all! do |ticket|
+      next unless @groups.include?(ticket.group_id)
+
+      users_with_existing_tickets.add(ticket.requester_id) if client.users.find!(id: ticket.requester_id)
+      delete_tickets_older_than_a_year(ticket)
+    end
+  end
+
+  # As we've already deleted tickets which have not been updated
+  # for a year (`tickets_for_deletion`), their associations to users
+  # should be gone too, meaning that `users_with_existing_tickets` will
+  # only be the ones who have have been active within a year.
+  def delete_old_users
+    @client.users.all! do |user|
+      if is_end_user?(user) &&
+          @users_with_existing_tickets.exclude?(user.id) &&
+          has_been_active_within_a_year?(user)
+
+        @users_to_delete.add(user.id)
+        if @dry_run
+          print '.'
+        else
+          user.destroy!
+        end
+      end
+    end
+  end
+
+private
+
+  def has_been_active_within_a_year?(user)
+    if user.last_login_at
+      return user.last_login_at < 1.year.ago.to_datetime
+    end
+
+    latest_update = user.updated_at || user.created_at
+    latest_update < 1.year.ago.to_datetime
+  end
+
+  def delete_tickets_older_than_a_year(ticket)
+    if not_updated_for_a_year?(ticket)
+      @tickets_to_delete.add(ticket.id)
+      if @dry_run
+        print '/'
+      else
+        ticket.destroy!
+      end
+    end
+  end
+
+  def not_updated_for_a_year?(ticket)
+    latest_update = ticket.updated_at || ticket.created_at
+    latest_update > 1.year.ago.to_datetime
+  end
+
+  def is_end_user?(user)
+    user.role.name == "end-user"
+  end
+end
