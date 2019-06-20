4f8486f003e54c76f0e83ad7fdfe576281220d65
David Pye <david.pye@digital.cabinet-office.gov.uk>
2019-06-20 15:16:27 +0100

    add delete by year bash scripts for running in parallel

---
+++ delete_tickets_2015.sh
@@ -0,0 +1,6 @@
+for raw_id in `cat y2015_tickets`
+do
+	clean_id=`echo $raw_id | sed -e 's/\,//g' | sed -e 's/\]//g' | sed -e 's/\[//g'`
+	curl $ZENDESK_URL/tickets/$clean_id -v -u $ZENDESK_USER_EMAIL:$ZENDESK_USER_PASSWORD -X DELETE
+	# echo "clean_id=$clean_id"
+done
