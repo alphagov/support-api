d1d879bbd4dcb3ba5df5d25d89ac667670a15a73
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2020-01-31 16:26:42 +0000

    Upload exported CSV file to S3 rather than attaching it

--- app/workers/generate_global_export_csv_worker.rb
+++ app/workers/generate_global_export_csv_worker.rb
@@ -8,6 +8,24 @@ class GenerateGlobalExportCsvWorker
       export_params["exclude_spam"],
     ).call
 
-    GlobalExportNotification.notification_email(export_params["notification_email"], filename, contents).deliver_now
+    s3_file = self.class.save_file_to_s3(filename, contents)
+
+    GlobalExportNotification.notification_email(export_params["notification_email"], s3_file.key).deliver_now
+  end
+
+  def self.save_file_to_s3(filename, csv)
+    connection = Fog::Storage.new(
+      provider: "AWS",
+      region: ENV["AWS_REGION"],
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    )
+
+    directory = connection.directories.get(ENV["AWS_S3_BUCKET_NAME"])
+
+    directory.files.create(
+      key: filename,
+      body: csv,
+    )
   end
 end
