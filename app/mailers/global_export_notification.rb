9279b6120a2fc1244d3b3be34ec8ac33a433e12d
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2020-01-31 16:26:42 +0000

    Upload exported CSV file to S3 rather than attaching it

--- app/mailers/global_export_notification.rb
+++ app/mailers/global_export_notification.rb
@@ -1,10 +1,9 @@
 class GlobalExportNotification < ApplicationMailer
-  def notification_email(notification_email, filename, csv_contents)
-    attachments[filename] = csv_contents
-
+  def notification_email(notification_email, url)
+    @url = url
     mail(
       to: notification_email,
-      subject: "[GOV.UK Feedback Explorer] Your global export is attached",
+      subject: "[GOV.UK Feedback Explorer] Your global export is ready",
     )
   end
 end
