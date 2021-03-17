78dae698b360c4faf60f638492aec326a35fd454
Thomas Leese <thomas@leese.io>
2021-03-17 10:54:42 +0000

    Remove code related to performance platform
    The performance platform has been shut down, so these pieces of code
    will stop working correctly. We don't have a suitable alternative set
    up, so we can remove the functionality from this app.

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,10 +7,6 @@ set :output, error: "log/cron.error.log", standard: "log/cron.log"
 job_type :rake, "cd :path && govuk_setenv support-api bundle exec rake :task :output"
 
 if ENV["SENTRY_CURRENT_ENV"] !~ /integration|staging/
-  every 1.month, at: "12:40 am" do
-    rake "performance_platform_uploads:push_problem_report_stats"
-  end
-
   every 1.day, at: "12:10 am" do
     rake "anonymous_feedback_deduplication:nightly"
   end
@@ -21,16 +17,6 @@ if ENV["SENTRY_CURRENT_ENV"] !~ /integration|staging/
 
   every 1.day, at: "12:30 am" do
     rake "service_feedback_aggregation:daily"
-  end
-
-  every 1.day, at: "1:30 am" do
-    # The service feedback aggregation task must have completed before this one,
-    # that's why we give it an hour to complete before triggering.
-    rake "performance_platform_uploads:push_service_feedback"
-  end
-
-  every 1.day, at: "1:40 am" do
-    rake "performance_platform_uploads:push_problem_report_daily_totals"
   end
 
   every 5.minutes, at: 3 do
