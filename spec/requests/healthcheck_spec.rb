6f11dc4fc5d6113c8dd11ae4ff73b59816557c51
Michael Walker <mike@barrucadu.co.uk>
2021-04-26 13:58:20 +0100

    Remove legacy /healthcheck route
    govuk-puppet and govuk-aws have now been updated to only use the new
    routes, so the old one is no longer needed.
    
    See RFC 141 for more information.

--- spec/requests/healthcheck_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-describe "healthcheck path" do
-  it "responds with 'OK'" do
-    get "/healthcheck"
-    expect(response).to be_successful
-    expect(response.body).to eq("OK")
-  end
-end
