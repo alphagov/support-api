289cf591bfc335264631366edd696c60d3495f7a
D Chua <deborah.chua@digital.cabinet-office.gov.uk>
2021-07-05 12:14:53 +0100

    Run rails app:update for 6.1
    This updates various configuration files

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,19 @@
 require_relative "boot"
 
+require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
+# require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,13 +23,20 @@ module SupportApi
   class Application < Rails::Application
     require "support_api"
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
 
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
     config.api_only = false
 
     # Disable Rack::Cache
+
     config.action_dispatch.rack_cache = nil
 
     config.active_record.schema_format = :sql
