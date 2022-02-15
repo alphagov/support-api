fb7b61d6d5948fbdbc13c9992a32ab610cf0811c
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2022-02-15 15:00:04 +0000

    Remove duplicate tests
    These tests appear to be identical to the ones further up/down
    in the file. This appears to have happened here:
    https://github.com/alphagov/support-api/blob/b0d88038089e9611c7a2cbbb96a3a88d0012a724/spec/workers/content_item_enrichment_worker_spec.rb

--- spec/workers/content_item_enrichment_worker_spec.rb
+++ spec/workers/content_item_enrichment_worker_spec.rb
@@ -31,27 +31,4 @@ describe ContentItemEnrichmentWorker do
       end
     end
   end
-
-  context "with an entry in the content store" do
-    before do
-      create(:gds)
-      stub_content_store_has_item(path)
-    end
-
-    context "without an existing content item" do
-      it "creates a new content item" do
-        expect(ContentItem.count).to eq(0)
-        worker.perform(problem_report.id)
-        expect(ContentItem.count).to eq(1)
-      end
-    end
-
-    context "with an existing content item" do
-      it "uses the existing content item" do
-        create(:content_item, path: path)
-
-        expect { worker.perform(problem_report.id) }.to_not(change { ContentItem.count })
-      end
-    end
-  end
 end
