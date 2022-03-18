d534deb53392e382bd4bdc036d7d3ae3affbbdfd
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2022-03-18 15:53:23 +0000

    Enable new Rails 7

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module SupportApi
     require "support_api"
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
