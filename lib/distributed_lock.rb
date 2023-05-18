c5a2a15b09079e4206925847e1acf05b5385a6d8
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-05-18 10:09:22 +0100

    Remove original lock now a safe time has passed - Remove obsolete brakeman referring to original lock.

--- lib/distributed_lock.rb
+++ lib/distributed_lock.rb
@@ -5,18 +5,15 @@ class DistributedLock
   LIFETIME = (10 * 60) # seconds
 
   def initialize(lock_name)
-    @lock_name = lock_name
     @full_lock_name = ActiveRecord::Base.sanitize_sql("support-api:#{lock_name}")
   end
 
   def lock
-    Redis.new.lock("support-api:#{Rails.env}:#{@lock_name}", life: LIFETIME) do
-      Redis.new.lock(@full_lock_name, life: LIFETIME) do
-        Rails.logger.debug("Successfully got a lock. Running...")
-        yield
-      end
+    Redis.new.lock(@full_lock_name, life: LIFETIME) do
+      Rails.logger.debug("Successfully got a lock. Running...")
+      yield
     end
   rescue Redis::Lock::LockNotAcquired => e
-    Rails.logger.debug("Failed to get lock for #{@lock_name} (#{e.message}). Another process probably got there first.")
+    Rails.logger.debug("Failed to get lock for #{@full_lock_name} (#{e.message}). Another process probably got there first.")
   end
 end
