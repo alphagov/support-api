aa09c926fc202be5eec38cfbb15c72a0b819441d
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2024-07-10 19:00:38 +0100

    Rename attributes in SupportTicket
    To distinguish better between Support API call attributes and Zendesk ticket attributes. Those are specific to the vendor and their API spec. It follows the pattern of PageImprovement.

--- app/controllers/support_tickets_controller.rb
+++ app/controllers/support_tickets_controller.rb
@@ -3,7 +3,7 @@ class SupportTicketsController < ApplicationController
     support_ticket = SupportTicket.new(attributes)
 
     if support_ticket.valid?
-      GDS_ZENDESK_CLIENT.ticket.create!(support_ticket.attributes)
+      GDS_ZENDESK_CLIENT.ticket.create!(support_ticket.zendesk_ticket_attributes)
 
       render json: { status: "success" }, status: :created
     else
