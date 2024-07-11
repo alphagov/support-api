ec450198614a01841546aa0073999c08768a663b
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2024-07-11 13:51:08 +0100

    Try this

--- app/controllers/support_tickets_controller.rb
+++ app/controllers/support_tickets_controller.rb
@@ -3,8 +3,12 @@ class SupportTicketsController < ApplicationController
     support_ticket = SupportTicket.new(support_ticket_attributes)
 
     if support_ticket.valid?
-      GDS_ZENDESK_CLIENT.ticket.create!(support_ticket.zendesk_ticket_attributes)
+      pp support_ticket.zendesk_ticket_attributes
 
+      ## TODO Handle Zendesk client's errors
+      response = GDS_ZENDESK_CLIENT.ticket.create!(support_ticket.zendesk_ticket_attributes)
+      pp response 
+      
       render json: { status: "success" }, status: :created
     else
       render json: { status: "error", errors: support_ticket.errors }, status: :unprocessable_entity
@@ -14,6 +18,12 @@ class SupportTicketsController < ApplicationController
 private
 
   def support_ticket_attributes
-    params.slice(:subject, :description, :tags, :user_agent, :priority, :requester, :collaborators, :custom_fields, :ticket_form_id)
+    params.slice(
+      :subject, :description, :tags, :priority, :requester, :collaborators, :custom_fields, :ticket_form_id
+    ).permit(
+      :subject, :description, :tags, :priority, :collaborators, :ticket_form_id,
+      requester: [:locale_id, :email, :name],
+      custom_fields: [:id, :value]
+    )
   end
 end
