2f9916322a01d63a20ddbd47f8320ebbbf9b9681
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2024-10-25 13:23:58 +0100

    Run `rails app:update`
    Manual changes/overrides:
    in development.rb
    - custom config.action_mailer.default_url_options
    - add 'Allow requests for all domains e.g. <app>.dev.gov.uk' back
    - don't Annotate rendered view with file names
    - add `config.active_record.yaml_column_permitted_classes = [Symbol, Date]` back
    
    in production.rb
    - keep custom config for `config.public_file_server.enabled`
    - don't Force all access to the app over SSL
    - add `config.action_mailer.delivery_method` back
    - add `config.action_mailer.notify_settings` back
    - add `config.active_record.yaml_column_permitted_classes = [Symbol, Date]` back
    
    in test.rb
    - custom config.action_mailer.default_url_options
    - add `config.active_record.yaml_column_permitted_classes = [Symbol, Date]` back
    
    in application.rb
    - add `require "support_api"` back
    - add `config.add_autoload_paths_to_load_path = true` back, although we should not be doing this
    - add `config.api_only = false` back
    - add 'Disable Rack::Cache' back
    - add `config.action_mailer.notify_settings` back
    
    - keep puma config from govuk_app_config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
+  # Skip http-to-https redirect for the default health check endpoint.
+  # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
+
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new($stdout)
     .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
@@ -45,7 +48,7 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
-  # Info include generic and useful information about system operation, but avoids logging too much
+  # "info" includes generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
   # want to log everything, set the level to "debug".
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
@@ -57,6 +60,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "support_api_production"
 
+  # Disable caching for Action Mailer templates even if Action Controller
+  # caching is enabled.
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
