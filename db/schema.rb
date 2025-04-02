cad43d661b5d812e56db2ccf28867595e5599dae
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2025-04-02 13:23:07 +0100

    Dump database schema after Rails upgrade
    This dumps the schema in a format output by Rails 8.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2019_01_30_105818) do
+ActiveRecord::Schema[8.0].define(version: 2019_01_30_105818) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "anonymous_contacts", id: :serial, force: :cascade do |t|
     t.string "type", limit: 255
@@ -105,5 +105,4 @@ ActiveRecord::Schema[7.1].define(version: 2019_01_30_105818) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
   end
-
 end
